362ad6028fbd8bc101459c6b36d23c8df89c406e
HosseinJavooni <h.javooni@faraboom.co>
2023-04-25 17:27:52 +0330

    commit_1402_02_05_1700pm

--- Quotes.sh
+++ Quotes.sh
@@ -1,6 +1,8 @@
 # !/bin/bash
+# Creation Data:  2023-04-25
 # THE USER WHO CREATE THIS FILE:  root
 # START # 
+
 VAR1="HosseinTest"
 FIRST_NAME=Javooni
 
@@ -12,5 +14,5 @@ echo 'Execute double quotes VAR1=$VAR1 FIRST_NAME=$FIRST_NAME whoami'
 
 #Reverse Quotes
 echo "Execute double quotes VAR1=$VAR1 FIRST_NAME=$FIRST_NAME `whoami`"
-
+ 
 # END #
